142db49c2e8d0c66f9e7e5e7d64429b909dfb262
Mate04 <matusmaldonado@hotmail.com>
2024-02-24 12:29:50 -0300

    yes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,14 +2,17 @@
 import '../style/navbar.css'
 import Link from 'next/link';
 import Dropdown from './dropdown';
-
+import Image from 'next/image';
+import logo from '../../public/logo.png'
 export default function NavBar(){
     const categorias = [null,'Shooter', 'Casual', 'Puzzle', 'Dress-up', 'Adventure'];
     return <>
     <nav className="navbar">
   <div className="containerr">
     <div className="titulo tituloOcti">
-      <Link href='/' className='tituloOcti-link modak-regular'>octiGames</Link>
+      <Link href='/' className='tituloOcti-link modak-regular'> 
+        <Image src={logo} width={125} alt='octiGames' className='tituloOcti-link-img'/>
+      </Link>
     </div>
     <ul>
       
