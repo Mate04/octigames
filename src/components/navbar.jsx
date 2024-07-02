767c89f276425df782f2ec9ba32acd3348bb7744
Pablo Jiménez <pablojimenez@MacBook-Pro-de-Pablo.local>
2024-07-02 15:41:10 -0600

    (Banner)

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -92,9 +92,7 @@ export default function NavBar() {
 
             </Link>
           </li>
-          {effect ==  '/game' ? <li>
-          <Banner width={320} height={50} keyAD={'6255d3c5fb923d4cc0f9a4c7fd594c5f'}/>
-        </li> : <></>}
+          {effect ==  '/game' ? <Banner width={320} height={50} keyAD={'6255d3c5fb923d4cc0f9a4c7fd594c5f'}/>: <></>}
           <li className='Categorias'>
             {Object.keys(categorias).map((categoria) => (
               <Link key={categoria} href={`#`} className='categoria'>
