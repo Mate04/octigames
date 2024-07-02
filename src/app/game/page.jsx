090cf8db8123bed6eff4a9f8154fad74e2556927
Pablo Jiménez <pablojimenez@MacBook-Pro-de-Pablo.local>
2024-07-02 14:53:27 -0600

    (App): Suspense GameComponent

--- src/app/game/page.jsx
+++ src/app/game/page.jsx
@@ -1,8 +1,11 @@
 "use client"
 import { GameComponent } from '../../components/gameCoponents';
+import { Suspense } from 'react'
 export default function Page() {
   return (
-    <GameComponent />
+    <Suspense>
+      <GameComponent />
+    </Suspense>
   )
 }
 
