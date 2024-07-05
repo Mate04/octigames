c90ee81b1f53aa7ef0b2be1803c117be09cdc95f
Pablo Jiménez <pablojimenez@MacBook-Pro-de-Pablo.local>
2024-07-05 10:43:02 -0600

    (GameView): Fixed mobile view

--- src/components/gameCoponents.jsx
+++ src/components/gameCoponents.jsx
@@ -1,35 +1,43 @@
 "use client"
-//public\WhatsApp Image 2024-02-04 at 16.11.28_e551de90.jpg'//'../../../public/WhatsApp Image 2024-02-04 at 16.11.28_e551de90.jpg'//public\WhatsApp Image 2024-02-04 at 16.11.28_e551de90.jpg
-import Script from 'next/script';
+import React, { useState, useEffect } from 'react';
+import { isMobile } from 'react-device-detect';
 import '../style/game.css'
-import Image from 'next/image';
-import logo from '../../public/logo.webp'
 import '../style/gampley.css'
 import Banner from '../components/banner';
-import Link from 'next/link'
 import LeftColumnView from '../components/LeftColumnView';
 import { useSearchParams } from 'next/navigation'
-// import Share from '@/components/share';
 import CategoriaV2 from '../components/CategoriaV2';
 export async function GameComponent() {
+  const [mobile, setMobile] = useState(false)
   const url = useSearchParams()
   const Md5 = url.get('Md5')
   const Title = url.get('Title')
+
+  useEffect(() => {
+    if (isMobile) {
+      setMobile(true)
+    }
+  }, [])
+
   return (
     <>
       <div className='container-games'>
-        <div className="square">
-          <LeftColumnView Md5={Md5} />
-        </div>
+        {!mobile &&
+          <div className="square">
+            <LeftColumnView Md5={Md5} />
+          </div>
+        }
         <div className="square">
           <iframe src={`https://html5.gamemonetize.co/${Md5}/`} width="100%" height="100%" scrolling="none" frameBorder="0" style={{ borderRadius: "10px" }}></iframe>
           <div className='info'>
             <p>{Title}</p>
           </div>
         </div>
-        <div id="rectangulo3" className="cuadrado rectangulo3">
-          <Banner keyAD={'80bb9c56da40368cfa31e6e1e2fda85e'} height={600} width={160} />
-        </div>
+        {!mobile ? (
+          <div id="rectangulo3" className="cuadrado rectangulo3">
+            <Banner keyAD={'80bb9c56da40368cfa31e6e1e2fda85e'} height={600} width={160} />
+          </div>
+        ) : <Banner keyAD={'6255d3c5fb923d4cc0f9a4c7fd594c5f'} width={320} height={50} />}
       </div>
       <CategoriaV2 numeroCategoria="17" nombreCategoria="Juegos para vos" />
     </>
